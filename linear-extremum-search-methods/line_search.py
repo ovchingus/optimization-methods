5b0f9299e95d7bbd0d68a147d39f0bfeafff09a9
Maksim <Lukianchukma@gmail.com>
2019-10-18 16:58:53 +0300

    --wip-- [skip ci]

---
+++ linear-extremum-search-methods/line_search.py
@@ -0,0 +1,27 @@
+import sys
+
+
+def line_search(fun, tolerance, max_iterations=100):
+    iteration = 0
+    x0 = 0
+    h = 0
+    xk = 0
+    x_next = float('-inf')
+    x_prev = 0
+
+    if fun(x0) > fun(x0 + tolerance):
+        xk = x0 + tolerance
+        h = tolerance
+    elif fun(x0) > fun(x0 - tolerance):
+        xk = x0 - tolerance
+        h = -tolerance
+
+    while iteration < max_iterations:
+        h *= 2
+        x_next = xk + h
+        if fun(xk) > fun(x_next):
+            x_prev = xk
+            xk = x_next
+        iteration += 1
+    return x_prev, x_next
+
--- linear-extremum-search-methods/main.py
+++ linear-extremum-search-methods/main.py
@@ -1,4 +1,5 @@
 from dichotomy import dichotomy
+from line_search import line_search
 from math import pi, cos
 
 
@@ -19,4 +20,6 @@ var2 = [function2, 0.0, pi]
 
 func, a, b = var3
 
-print(dichotomy(func, a, b, 0.001, 5))
+#print(dichotomy(func, a, b, 0.001, 5))
+
+print(line_search(func, 0.001))

18de3fbb9647223128025822b8d73a6b467717d2
Maksim <Lukianchukma@gmail.com>
2019-10-19 15:03:06 +0300

    --wip-- [skip ci]

--- linear-extremum-search-methods/golden_section.py
+++ linear-extremum-search-methods/golden_section.py
@@ -1,12 +1,13 @@
 import math
-from util import gr
+from .util import gr
 
 '''python program for golden section search.  This implementation
    does not reuse function evaluations and assumes the minimum is c
    or d (not on the edges at a or b)'''
 
-def golden_section(f, a, b, tolerance = 1e-5, max_iterations = 100):
-    '''
+
+def golden_section(f, a, b, tolerance=1e-5, max_iterations=100):
+    """
     golden section search
     to find the minimum of f on [a,b]
     f: a strictly unimodal function on [a,b]
@@ -17,14 +18,15 @@ def golden_section(f, a, b, tolerance = 1e-5, max_iterations = 100):
     >>> x
     2.000009644875678
 
-    '''
+    """
+
     iteration = 0
     c = b - (b - a) / gr
-    d = a + (b - a) / gr 
+    d = a + (b - a) / gr
 
     while iteration < max_iterations:
         iteration += 1
-        
+
         if abs(c - d) < tolerance:
             return (b + a) / 2
 
